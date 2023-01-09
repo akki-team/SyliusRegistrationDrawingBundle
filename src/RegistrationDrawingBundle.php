91698dc30c6400eccb5a7488c535befd06ceff7c
amadou <amadou@akki.fr>
2023-01-09 12:04:43 +0100

    Adding plugin trait

--- src/RegistrationDrawingBundle.php
+++ src/RegistrationDrawingBundle.php
@@ -2,9 +2,10 @@
 
 namespace Akki\SyliusRegistrationDrawingBundle;
 
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class RegistrationDrawingBundle extends Bundle
 {
-
+    use SyliusPluginTrait;
 }
