ae91b176ef1599664140082256f7604af2e50256
amadou <amadou@akki.fr>
2023-02-15 12:51:22 +0100

    Generate migrations

--- src/Migrations/Version20230215114743.php
+++ src/Migrations/Version20230215114743.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230206150421 extends AbstractMigration
+final class Version20230215114743 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20230206150421 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE registration_drawing ADD sender VARCHAR(255) NOT NULL, ADD recipients LONGTEXT NOT NULL');
+        $this->addSql('ALTER TABLE registration_drawing ADD password VARCHAR(255) DEFAULT NULL, DROP sender');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE registration_drawing DROP sender, DROP recipients');
+        $this->addSql('ALTER TABLE registration_drawing ADD sender VARCHAR(255) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`, DROP password');
     }
 }
